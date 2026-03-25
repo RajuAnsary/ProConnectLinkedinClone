8d55a2972bba47f3bd75b93ec8d37b301ee391b4
Raju Ansary <raju@ansary.com>
2026-03-25 23:08:28 +0530

    Update frontend to use deployed backend URL

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 ﻿import axios from "axios";
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:9090";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://proconnectlinkedinclone-lldr.onrender.com";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
